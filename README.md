95dd9d28cfd42bdaf2b5f4eaebb2b72da040a8dc
newbee14 <ashishpandey01@yahoo.com>
2019-05-17 00:43:38 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Repo Issue Counter
+
+# Question
 - Total number of open issues
 - Number of open issues that were opened in the last 24 hours
 - Number of open issues that were opened more than 24 hours ago but less than 7 days ago
@@ -6,15 +8,24 @@
 
 # Solution!
 
-  - I used Github Rest API v3 for get the required data.
-  - Basic info to the API with repo name gives us the total number of open issues.
-  - Then a call to /issues with necesary flags gives all the issues but in a page order i.e.  page 1 has 100, page 2 has next 100 and so on.
-  - All the calls are i async in nature so I started 4 parallel processes for first 4 pages. If there are are no more issues the pages return undefined. But in case in first call we get 400 issues then we recursively start 2 parallel processes for as long as there are issues.
-  - For calculating dates I have used moments.js
-  - This code is written Node.js and I've used fatify as the middleware.
+  - I used Github Rest API v3 for getting the required data.
+  - Calling API with repo name gives us the total number of open issues with pull requests.
+  - Calling Search API with repo name and type PR gives us the total number Pull Request, from which we can get total open issues.
+  - Calling search API to get to get open issues for given time intervals.
+  - For calculating dates I have used moments.js.
+  - This code is written Node.js and I've used fatify as web framework.
 
 ### Future Work
-Github's API v4 is using GraphQL which in some blog posts is claimed to be fater than rest. So if given more time I would have tried ti implement GraphQL.
-The API itself is very low performing there isn't much can be done the way we pull the data minor tweaks can surely improve the performance a bit in my view there cant be any considerable improvement. 
+Github's API v4 is using GraphQL which in some blog posts is claimed to be fater than REST. So if given more time I would have tried to implement GraphQL.
 
+# Important!
+To run this you will have to give your Auth token at /api/github.js line 4. Which you can generate from settings/Developer settings/personal acess token.
 
+# How to start
+  - Env- Nodejs, npm
+  - Clone the repo
+  - npm install
+  - node index.js
+  - open index.html
+  - Enter repo URL
+  
--- api/github.js
+++ api/github.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const moment = require('moment');
 
-const TOKEN = 'a25a0f185463d850849052d2a20a742c07d45099'; //github token for Authentication
+const TOKEN = ''; //github token for Authentication
 const defaultFetchOptions = {
 	method: 'get',
 	headers: {
@@ -10,40 +10,19 @@ const defaultFetchOptions = {
 };
 //fetch call to github apis
 const getFetchRequest = async (url, method = 'get', options = defaultFetchOptions) => {
-	return fetch(url, { method: method, ...options });
+	console.log(url);
+	return fetch(url, { method: method, ...options }).then((response) => response.json());
 };
-/* 
-	async function called my main function and returns promise
-	one first run it creates 4 async requests and still if all the issues have not been fetched it
-	recursively creates 2 requests as loong as all the issues have not been fetched
-*/
-const getIssues = async (issueURL, since, page = 1, lastPage = 4) => {
-	const max = lastPage - page + 1;
-	//waits for all the results of the apis calls made have been fetched
-	const responses = await Promise.all(
-		[...Array(max)].map((x, i) => {
-			return getFetchRequest(`${issueURL}&page=${page + i}&since=${new Date(since).toISOString()}`);
-		})
-	);
-	//resolves the promise and concats all the results
-	const data = await Promise.all(responses.map((res) => res.json()));
-	//console.log(data);
-	let finalData = [].concat.apply([], data);
-	//if all the issues have not been fetched then we make recursive calls to fetch all data
-	if (finalData.length === max * 100) {
-		finalData = finalData.concat(await getIssues(issueURL, since, lastPage + 1, lastPage + 3));
-	}
-
-  return new Promise((resolve) => {
-    resolve(finalData);
-  });
+//function to fetch issues
+const getIssues = async (issueURL, since) => {
+	const responses = await getFetchRequest(`${issueURL}+created:%3E=${moment(since).format('YYYY-MM-DDTHH:mm:ss')}%2B05:30`);
+	return responses;
 };
 
 async function githubRoutes(fastify) {
 	
 		fastify.get('/', async (request, reply) => {
 			let repo = request.query.url || '';
-			//check is query param is there
 			if (!repo) {
 				return {
 					error: true,
@@ -64,22 +43,37 @@ async function githubRoutes(fastify) {
 				};
 			}
 			//prepare API URL
+			const prURL = `https://api.github.com/search/issues?q=+is:pr+repo:${user}/${repoName}+is:open`;
 			const repoURL = `https://api.github.com/repos/${user}/${repoName}`;
-			const issueURL = `https://api.github.com/repos/${user}/${repoName}/issues?state=open&per_page=100`;
+	
+			//const issueURL = `https://api.github.com/repos/${user}/${repoName}/issues?state=open&per_page=100`;
+			const issueURL= `https://api.github.com/search/issues?q=+is:issue+repo:${user}/${repoName}+is:open`;
 			const apiCalls = [];
 			// Parallel API calls
+			console.log("repoURL"+"      "+repoURL);
+			
 			try {
-				apiCalls.push(fetch(repoURL, defaultFetchOptions).then((response) => response.json()), getIssues(issueURL, moment().subtract(1, 'day')), getIssues(issueURL, moment().subtract(7, 'day')));
+				apiCalls.push(
+					getFetchRequest(repoURL, defaultFetchOptions), 
+					getIssues(issueURL, moment().subtract(1, 'day')), 
+					getIssues(issueURL, moment().subtract(7, 'day')),
+					getFetchRequest(prURL, defaultFetchOptions), 
+					
+					);
+				
 				const data = await Promise.all(apiCalls);
+				//console.log(data);
+				
 				if (data[0].message && data[0].message === 'Not Found') {
        				return {
          				error: true,
          				message: 'Invalid Repo Address'
        				}		 
 				}
-				const openIssueCount = data[0].open_issues_count || 0;
-				const lastDayOpenIssueCount = openIssueCount === 0 ? 0 : data[1].length;
-				const lastSevenDaysOpenIssueCount = openIssueCount === 0 ? 0 : data[2].length - lastDayOpenIssueCount;
+				
+				const openIssueCount =data[0].open_issues_count - data[3].total_count || 0;
+				const lastDayOpenIssueCount = openIssueCount === 0 ? 0 : data[1].total_count;
+				const lastSevenDaysOpenIssueCount = openIssueCount === 0 ? 0 : data[2].total_count - lastDayOpenIssueCount;
 				const moreThanSevenDays = openIssueCount === 0 ? 0 : openIssueCount - lastSevenDaysOpenIssueCount - lastDayOpenIssueCount;
 				return { repoOwner: user, repoUrl: repo, repoName, openIssueCount, lastDayOpenIssueCount, lastSevenDaysOpenIssueCount, moreThanSevenDays };
 			} catch (e) {
